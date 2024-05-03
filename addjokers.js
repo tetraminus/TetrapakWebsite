f1b6320e8ef0f512552037b437acdf1172813bfb
tetraminus <54343976+tetraminus@users.noreply.github.com>
2024-05-03 11:45:36 -0700

    Initial commit

---
+++ addjokers.js
@@ -0,0 +1,164 @@
+
+
+let cols = {
+  
+  MULT: "#FE5F55",
+  CHIPS: "#009dff",
+  MONEY: "#f3b958",
+  XMULT: "#FE5F55",
+  FILTER: "#ff9a00",
+  ATTENTION: "#ff9a00",
+  BLUE: "#009dff",
+  RED: "#FE5F55",
+  GREEN: "#4BC292",
+  PALE_GREEN: "#56a887",
+  ORANGE: "#fda200",
+  IMPORTANT: "#ff9a00",
+  GOLD: "#eac058",
+  YELLOW: "#ffff00",
+  CLEAR: "#00000000", 
+  WHITE: "#ffffff",
+  PURPLE: "#8867a5",
+  BLACK: "#374244",
+  L_BLACK: "#4f6367",
+  GREY: "#5f7377",
+  CHANCE: "#4BC292",
+  JOKER_GREY: "#bfc7d5",
+  VOUCHER: "#cb724c",
+  BOOSTER: "#646eb7",
+  EDITION: "#ffffff",
+  DARK_EDITION: "#5d5dff",
+  ETERNAL: "#c75985",
+  INACTIVE: "#ffffff99",
+  HEARTS: "#f03464",
+  DIAMONDS: "#f06b3f",
+  SPADES: "#403995",
+  CLUBS: "#235955",
+  ENHANCED: "#8389DD",
+  JOKER: "#708b91",
+  TAROT: "#a782d1",
+  PLANET: "#13afce",
+  SPECTRAL: "#4584fa",
+  VOUCHER: "#fd682b",
+  EDITION: "#4ca893",
+}
+
+let rarities = {
+  "Common": "#009dff", 
+  "Uncommon": "#4BC292",
+  "Rare": "#fe5f55",
+  "Legendary": "#b26cbb",
+  "Curse": "#28142a",
+  "Joker": "#708b91",
+  "Tarot": "#a782d1",
+  "Planet": "#13afce",
+  "Spectral": "#4584fa",
+  "Voucher": "#fd682b",
+  "Pack": "#9bb6bd",
+  "Enhancement": "#8389DD",
+  "Edition": "#4ca893",
+  "Seal": "#4584fa",
+  "Deck": "#9bb6bd",
+  "Sticker": "#5d5dff",
+  "Boss Blind": "#5d5dff",
+  "Showdown": "#4584fa",
+}
+
+
+regex = /{([^}]+)}/g;
+
+let add_cards_to_div = (jokers, jokers_div) => {
+  for (let joker of jokers) {
+    console.log("adding joker", joker.name);
+  
+    joker.text = joker.text.map((line) => { return line + "{}"});
+  
+    joker.text = joker.text.join("<br/>");
+    joker.text = joker.text.replaceAll("{}", "</span>");
+    joker.text = joker.text.replace(regex, function replacer(match, p1, offset, string, groups) {
+      let classes = p1.split(",");
+  
+      let css_styling = "";
+  
+      for (let i = 0; i < classes.length; i++) {
+        let parts = classes[i].split(":");
+        if (parts[0] === "C") {
+          css_styling += `color: ${cols[parts[1].toUpperCase()]};`;
+        } else if (parts[0] === "X") {
+          css_styling += `background-color: ${cols[parts[1].toUpperCase()]}; border-radius: 5px; padding: 0 5px;`;
+        }
+      }
+  
+      return `</span><span style='${css_styling}'>`;
+    });
+  
+    let joker_div = document.createElement("div");
+    joker_div.classList.add("joker");
+    if (joker.rarity === "Sticker" || joker.rarity == "Seal") {
+      joker_div.innerHTML = `
+        <h3>${joker.name}</h3>
+        <img src="${joker.image_url}" alt="${joker.name}" class="hasback" />
+        <h4 class="rarity" style="background-color: ${rarities[joker.rarity]}">${joker.rarity}</h4>
+        <div class="text">${joker.text}</div>
+      `;
+    } else if (joker.soul) {
+      joker_div.innerHTML = `
+        <h3>${joker.name}</h3>
+        <span class="soulholder">
+          <img src="${joker.image_url}" alt="${joker.name}" class="soul-bg" />
+          <img src="${joker.image_url}" alt="${joker.name}" class="soul-top" />
+        </span>
+        <h4 class="rarity" style="background-color: ${rarities[joker.rarity]}">${joker.rarity}</h4>
+        <div class="text">${joker.text}</div>
+      `;
+    } else {
+      joker_div.innerHTML = `
+        <h3>${joker.name}</h3>
+        <img src="${joker.image_url}" alt="${joker.name}" />
+        <h4 class="rarity" style="background-color: ${rarities[joker.rarity]}">${joker.rarity}</h4>
+        <div class="text">${joker.text}</div>
+      `;
+    }
+  
+    jokers_div.appendChild(joker_div);
+  }
+}
+
+// if (jokers.length === 0) {
+//   document.querySelector(".jokersfull").style.display = "none"
+// }
+// else {
+//     let jokers_div = document.querySelector(".jokers");
+//     add_cards_to_div(jokers, jokers_div);
+// }
+
+// add all catagoires in data
+for (let category in data) {
+  let jokers = data[category];
+
+  // div template
+  //<div class="jokersfull">
+  //       <h2>jokers</h2>
+  //       <br>
+  //       <div class="jokers cards">
+  //
+  //       </div>
+  //       <br>
+  //     </div>
+
+    let jokers_div = document.createElement("div");
+    jokers_div.classList.add(category + "full");
+    //add under "main" div
+    document.querySelector("main").appendChild(jokers_div);
+    let h2 = document.createElement("h2");
+    h2.innerText = category;
+    jokers_div.appendChild(h2);
+    let br = document.createElement("br");
+    jokers_div.appendChild(br);
+    let jokers_cards = document.createElement("div");
+    jokers_cards.classList.add(category);
+    jokers_cards.classList.add("cards");
+    jokers_div.appendChild(jokers_cards);
+
+  add_cards_to_div(jokers, jokers_cards);
+}
